0e0ab1b2e3b60a0e75cecd5985a9b49adbf9ed06
Mike Potter <mike_potter@westbounddesigns.com>
2026-03-03 17:55:10 -0500

    Created Solid Initial UI

--- src/app.py
+++ src/app.py
@@ -1,16 +1,23 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Label
+from textual.widgets import Header, Footer, Label
+from textual.containers import Container, Horizontal, VerticalScroll
 
 class veritrakk(App):
     CSS_PATH = "veritrakk.tcss"
     BINDINGS = [("q", "quit", "Quit")]
+    TITLE = "WELCOME TO VERITRAKK"
 
     def compose(self) -> ComposeResult:
-        
-        TEXT = "THESE ARE WORDS"
-        label = Label(TEXT)
-        label.border_title = "WELCOME TO VERITRAKK"
-        yield(label)
+
+        with Horizontal():
+            left = Container(id="left")
+            left.border_title = "SELECT PROCESSES"
+            yield left
+            yield Container(id="right")
+            
+        yield Header()
+        yield Footer()
+      
  
 
 veritrakk().run()
