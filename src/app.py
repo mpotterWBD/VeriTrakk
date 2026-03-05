cf3112c31bb49d31ccc42bf8b2b41de7eb545eec
Mike Potter <mike_potter@westbounddesigns.com>
2026-03-04 21:40:27 -0500

    Added screens instead of hidding/showing, fixed when b is pressed to go to default select

--- src/app.py
+++ src/app.py
@@ -1,58 +1,79 @@
 from textual.app import App, ComposeResult
 from textual.widgets import Header, Footer, Label, Select, Rule
 from textual.binding import Binding
+from textual.screen import Screen
 from textual.containers import Container, Horizontal, VerticalScroll
 from .storage import file_parser
 
-class veritrakk(App):
-    CSS_PATH = "veritrakk.tcss"
-    BINDINGS = [
-        Binding("q", "quit", "Quit"),
-        Binding("b", "back", "Back")
-    ]
-
-
+class MainScreen(Screen):
     TITLE = "WELCOME TO VERITRAKK"
 
-    def compose(self) -> ComposeResult:
+    def compose(self)-> ComposeResult:
         yield Header()
+        with Horizontal():
+            
+            select_cont = Container(id="select_cont")
+            select_cont.border_title = "SELECT PROCESSES"
+            with select_cont:
 
-        yield Rule(line_style="double", id="header_rule")
+                options = [(x,x) for x in file_parser()]
+                select_proc = Select(options,id="process_select",compact=True,prompt="Select a process",allow_blank=True)
+                yield select_proc
+                 
+        yield Footer()
 
+    def on_screen_resume(self) -> None:
+        select = self.query_one("#process_select", Select)
+        select.clear()
+        
+        # select.value = Select.BLANK
+
+    # def on_screen_resume(self):
+    #     select = self.query_one("#process_select", Select)
+    #     select.value = Select.BLANK
+
+class ProcessScreen(Screen):
+    
+    TITLE = "WELCOME TO VERITRAKK"
+
+    BINDINGS = [
+        Binding("b", "back", "Back"),
+    ]
+  
+    def compose(self):
+        yield Header(id="compose_header")
         with Horizontal():
             stage_proc_l = Container(id="stage_proc_l")
             stage_proc_r = Container(id="stage_proc_r")
             stage_proc_l.border_title = "PROCESS TREE"
             stage_proc_r.border_title = "PROCESS DETAILS"
-
             yield stage_proc_l
             yield stage_proc_r
-            select_cont = Container(id="select_cont")
-            select_cont.border_title = "SELECT PROCESSES"
-            with select_cont:
-
-                select_proc = Select(((x,x) for x in file_parser()),id="process_select",compact=True)
-                yield select_proc
-         
+        
         yield Footer()
-    
-    def on_select_changed(self, event: Select.Changed) -> None:
-        if(event.select.id == "process_select"):
-            self.query_one("#select_cont").display = "none"
-            self.query_one("#stage_proc_l").display = "block"
-            self.query_one("#stage_proc_r").display = "block"
-        # if(self.query_one("#process_select").value != None):
 
     def action_back(self) -> None:
-        select_proc = self.query_one("#process_select")
-        left = self.query_one("#select_cont")
-        left.display = "block"
-        self.query_one("#stage_proc_l").display = "none"
-        self.query_one("#stage_proc_r").display = "none"
-        self.call_after_refresh(select_proc.focus)
+        self.app.pop_screen()
+
+class veritrakk(App):
+    CSS_PATH = "veritrakk.tcss"
+
+    BINDINGS = [
+        Binding("q", "quit", "Quit"),
+    ]
+
+    SCREENS = {
+        "main_screen": MainScreen,
+        "process_screen": ProcessScreen,
+    }
+
+    def on_select_changed(self, event: Select.Changed) -> None:
+        if(event.select.is_blank()):
+            return
+        if(event.select.id == "process_select"):
+            self.app.push_screen("process_screen")
 
     def on_mount(self) -> None:
-        self.query_one("#stage_proc_l").display = "none"
-        self.query_one("#stage_proc_r").display = "none"
+        self.push_screen(MainScreen())
       
 veritrakk().run()
