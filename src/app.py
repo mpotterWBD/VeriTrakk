0f1fff5490bf613c3d36e7e42e12dea744a52e06
Mike Potter <mike_potter@westbounddesigns.com>
2026-03-06 16:59:39 -0500

    Adjusted UI to something that is workable for now, added comments for organization, started putting .tcss items into alphabetical order

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from textual.app import App, ComposeResult
 from textual.widgets import Header, Footer, Label, Select, Rule, ContentSwitcher, Placeholder
 from textual.binding import Binding
 from textual.screen import Screen
-from textual.containers import Container, Horizontal, VerticalScroll
+from textual.containers import Container, Horizontal, VerticalScroll, Vertical
 from .storage import file_parser, number_of_files
 
 FILES = file_parser()
@@ -18,21 +18,39 @@ class MainScreen(Screen):
     def compose(self)-> ComposeResult:
         yield Header()
 
-        with Horizontal():
-            with ContentSwitcher(initial="select_cont",id="ms_content_switcher"):
-                with Container(id="select_cont"):   
+        with Vertical():
 
-                    options = [(x,x) for x in FILES]
-                    yield Select(
-                        options,
-                        id="process_select",
-                        compact=True,
-                        prompt="Select",
-                        allow_blank=True
-                    )
-                with Container(id="process_cont"):
-                    yield Placeholder("PLACEHOLDER")
-                         
+#TABS START
+#--------------------------------------------------------------------------------------
+            with Container(id="tab_placeholder"):
+                yield Placeholder("SELECT TABS GO HERE")
+#TABS END
+#--------------------------------------------------------------------------------------
+
+            with Horizontal():
+#CONTENTSWITCHER START
+#--------------------------------------------------------------------------------------
+                with ContentSwitcher(initial="select_cont",id="ms_content_switcher"):
+#SELECT CODE START
+#--------------------------------------------------------------------------------------
+                    with Container(id="select_cont"):   
+
+                        options = [(x,x) for x in FILES]
+                        yield Select(
+                            options,
+                            id="process_select",
+                            compact=True,
+                            prompt="Select",
+                            allow_blank=True
+                        )
+#SELECT CODE END
+#--------------------------------------------------------------------------------------
+                    with Container(id="process_cont"):
+                        yield Placeholder("TREE GOES HERE")
+#CONTENTSWITCHER END
+#--------------------------------------------------------------------------------------
+                with Container(id="process_builder"):
+                    yield Placeholder("PROCESS BUILDER GOES HERE")            
         yield Footer()
 
     def action_back(self) -> None:
@@ -46,7 +64,11 @@ class MainScreen(Screen):
         select_cont.styles.height=NOF+4
 
         process_cont = self.query_one("#process_cont", Container)
-        process_cont.border_title = "PROCESS DETAILS"
+        process_cont.border_title = "PROCESS TREE"
+
+        process_builder = self.query_one("#process_builder")
+        process_builder.border_title = "PROCESS BUILDER"
+        process_builder.styles.height=NOF+4
 
     def on_screen_resume(self) -> None:
         select = self.query_one("#process_select", Select)
