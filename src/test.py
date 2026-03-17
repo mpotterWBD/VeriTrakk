f21039c3b476d8b8891e00e1fe52a9ff3d5d8a75
Mike Potter <mike_potter@westbounddesigns.com>
2026-03-17 12:24:02 -0400

    Ready to Merge

--- src/test.py
+++ src/test.py
@@ -1,6 +1,11 @@
-from storage import file_parser, number_of_files, file_reader
+from storage import file_parser, number_of_files, file_reader, save_success
 print("TESTING...")
 
-data = file_reader("test_proc.prcss")
-for x in data:
-    print(x[0])
+# data = file_reader("test_proc.prcss")
+# for x in data:
+#     print(x[0])
+
+file = "AutoFilter_Mechanical_Design.prcss"
+label = "<Pull New PN#\n"
+
+save_success(label, file)
